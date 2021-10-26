02a328bc1a85a16c38b38a3d13f2d355ce4b85f3
rohith <rohith3085@gmail.com>
2021-10-26 00:35:45 +0000

    Knowing about strings

---
+++ string.py
@@ -0,0 +1,15 @@
+print("TO day is a good day to learn python") # with double quotes
+print('You will rock it ')                    # with single quotes
+print("Peter's mother is a teacher")          
+print('We can include "Quotes" even in a string')
+
+#sting concatenation 
+print("Hello " + " programmers")
+#variable adding 
+Greatting = "hello"
+Name = "rohith"
+# if you want spaces in between you can add it 
+print(Greatting + " " + Name)
+#input fuction 
+name = input("Enter your name: ")
+print(Greatting+ " " + name)
