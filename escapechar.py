eb0b192ac8c5e38ecabfb7a355ee8d4fd8f25ac8
rohith <rohith3085@gmail.com>
2021-10-27 17:02:31 +0000

    About escape character in python

---
+++ escapechar.py
@@ -0,0 +1,18 @@
+splitstring = "This lines has\nbeen split\nover into \nseveral\nlines"
+print(splitstring)
+
+print(" ")                     #this line is for leaving a gap 
+
+tabstring = "1\t2\t3\t4\t5"
+print(tabstring)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+anothersplitsting = """The pet shop owner said \
+"NO, no, \
+'e's uh,...he's \
+resting"."""
+
+print(anothersplitsting)
