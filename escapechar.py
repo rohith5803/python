42026f99a85dcaef0424376a80fdbdf1c267afcf
rohith <rohith3085@gmail.com>
2021-10-28 06:27:25 +0000

    To know more about escape character in python

--- escapechar.py
+++ escapechar.py
@@ -7,7 +7,9 @@ tabstring = "1\t2\t3\t4\t5"
 print(tabstring)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#or 
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+#or 
 print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
 
 anothersplitsting = """The pet shop owner said \
@@ -16,3 +18,6 @@ anothersplitsting = """The pet shop owner said \
 resting"."""
 
 print(anothersplitsting)
+
+print("c:\\user\\rohith\\movies\\spiderman")
+print(r"c:\user\rohith\movies\spiderman")    # r refers to raw that print the line as given 
