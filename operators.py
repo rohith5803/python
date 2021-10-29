3f6ce58a0cfd98ee8359428aae7d0aec05447774
rohith <rohith3085@gmail.com>
2021-10-29 09:15:37 +0000

    Python operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+print(a+b)      #15 
+print(a-b)      #10
+print(a*b)      #50 
+print(a/b)      #2.0
+print(a//b)     #2 
+print(a%b)      #0 
+
+print()
+
+for i in range(1,5):   # for loop it starts from 1 and it ends with 4 because it consider has n-1 (last value - 1)
+    print(i)
