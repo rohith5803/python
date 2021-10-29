b461ede4b017a2aefbe1d1245ebcab9041091cb1
rohith <rohith3085@gmail.com>
2021-10-29 11:35:12 +0000

    Indexing

---
+++ str_data.py
@@ -0,0 +1,23 @@
+         #01234567890123
+parrot = "NORWEGIEN BLUE "
+print(parrot)
+print(parrot[3])   # W 
+print(parrot[11])  # L
+print(parrot[9])   # space 
+
+#small problem to solve 
+#output should be 
+# NORWEGIEN BLUE 
+#   W 
+#   E
+#
+#   W
+#   I 
+#   N
+#
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
