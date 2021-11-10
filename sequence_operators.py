20f7b1e49b30474592ca42a97bd3d225f59e581f
rohith <rohith3085@gmail.com>
2021-11-10 05:20:29 +0000

    its all about strings in python

---
+++ sequence_operators.py
@@ -0,0 +1,15 @@
+string1 = "I'm not "
+string2 = "trying to "
+string3 = "impress anyone"
+print(string1+string2+string3)
+print("I'm not" " trying to" " impress anyone")
+print("believe it\n" *10)
+print()
+print("believe it\n"*(5+5))
+print()
+today = "tuesday"
+print("tue" in today)     #true
+print("day" in today)     #true
+print('t' in today)       #true
+print("mon" in today)     #false
+print("rohith" in today)  #false
